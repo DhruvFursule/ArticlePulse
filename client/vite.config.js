a5e6bb6984416cedb7d0e813cc15e61d1bae20fe
DhruvFursule <dhruv.fursule@gmail.com>
2025-09-25 22:30:12 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ArticlePulse-Project/",
+  base: "/",
   // server : {host : true}
 })
